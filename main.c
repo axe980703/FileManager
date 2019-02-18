275fd8bcc0c38eaeedfaf64ddd08a9c81e0917d2
Курбан <kurban_mamaev@mail.ru>
2019-02-19 01:21:52 +0500

    added: implementation of push child/obj and init child/obj

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
+/*#include <stdio.h>
 #include "os_file.h"
-#include <stdio.h>
 
 int main()
 {
 
     file_manager_t fm;
     setup_file_manager(&fm);
-    int result;
-    result = fm.create(10);
-    result = fm.destroy();
+
+
 
 
     return 0;
 }
+*/
--- task.c
+++ task.c
@@ -3,31 +3,83 @@
 
 #define NULLDIR -1
 #define ROOT 0
+#define INIT_MEMORY 16
 
 typedef struct Object Object;
 typedef struct Manager Manager;
 
 struct Object {
-    char* name;
+    char *name;
     int isFile;
     int size;
     int parentID;
     int *childID;
     int childCnt;
+    int childMem;
 };
 
 struct Manager {
     Object *obj;
     int objCnt;
+    int objMem;
     int capacity;
     int curDir;
 };
 
 Manager mn;
 
+void init_child(Object *ob) {
+    ob->childMem = INIT_MEMORY;
+    ob->childCnt = 0;
+    ob->childID = (int*) malloc(sizeof(int) * ob->childMem);
+}
+
+void init_obj() {
+    mn.capacity = 0;
+    mn.curDir = NULLDIR;
+    mn.objMem = 4;
+    mn.objCnt = 0;
+    mn.obj = (Object*) malloc(sizeof(Object) * mn.objMem);
+}
+
+///// destroy child/obj and include them into rmv child/obj  THINK ABOUT EFFECTIVE MEMORY MANAGING
+
+void push_child(Object *ob, int x) {
+    if(ob->childCnt == ob->childMem) {
+        ob->childMem *= 2;
+        int *tmp = (int*) malloc(sizeof(int) * ob->childMem);
+        for(int i = 0; i < ob->childMem / 2; i++)
+            tmp[i] = ob->childID[i];
+        free(ob->childID);
+        ob->childID = tmp;
+    }
+    ob->childID[ob->childCnt++] = x;
+}
+
+void push_obj(Object obj) {
+    if(mn.objCnt == mn.objMem) {
+        mn.objMem *= 2;
+        Object *tmp = (Object*) malloc(sizeof(Object) * mn.objMem);
+        for(int i = 0; i < mn.objMem / 2; i++)
+            tmp[i] = mn.obj[i];
+        free(mn.obj);
+        mn.obj = tmp;
+    }
+    mn.obj[mn.objCnt++] = obj;
+}
+
+void rmv_child() {
+
+}
+
+void rmv_obj() {
+
+}
 
 int createFM(int disk_size)
 {
+    if(mn.curDir != NULLDIR)
+        return 0;
 
     return 1;
 }
@@ -43,12 +95,31 @@ void my_get_cur_dir(char* dst) {
 }
 
 void setup_file_manager(file_manager_t *fm) {
-    mn.capacity = 0;
-    mn.curDir = NULLDIR;
-    mn.objCnt = 0;
+
     fm->create = createFM;
     fm->destroy = my_destroy;
     fm->get_cur_dir = my_get_cur_dir;
 
 }
 
+int main()
+{
+    file_manager_t fm;
+    setup_file_manager(&fm);
+
+    init_obj();
+    Object ob1;
+    ob1.name = "ob1";
+    init_child(&ob1);
+    push_child(&ob1, 12);
+    push_child(&ob1, 13);
+    push_child(&ob1, 322);
+    push_obj(ob1);
+    push_obj(ob1);
+    push_obj(ob1);
+    printf("%d\n", mn.obj[1].childID[2]);
+    ob1 = mn.obj[2];
+    printf("%s", ob1.name);
+
+    return 0;
+}
