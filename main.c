b151bf71291f13bdc3d218f84ae12b2d9615d116
Курбан <kurban_mamaev@mail.ru>
2019-02-28 01:15:51 +0500

    completed

--- main.c
+++ main.c
@@ -3,6 +3,21 @@
 
 int main()
 {
+    file_manager_t fm;
+    setup_file_manager(&fm);
+    fm.create(24);
+    //destroyFM();
+    printf("%d", fm.create_dir("a"));
+    printf("%d", fm.create_dir("a/b"));
+    printf("%d", fm.create_file("a/b/c", 12));
+    printf("%d", fm.create_dir("a/b/d"));
+    printf("%d", fm.create_dir("a/b/e"));
+    printf("%d", fm.create_dir("a/b/d/f"));
+    printf("%d", fm.create_file("a/b/c.mp3", 2));
+    printf("%d\n", fm.change_dir("/a/b/d"));
+    char buf[50];
+    fm.get_cur_dir(buf);
+    printf("%s", buf);
 
     return 0;
 }
